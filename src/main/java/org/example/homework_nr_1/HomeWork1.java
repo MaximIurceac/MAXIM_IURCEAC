76c095d892de1325d8ffd6c2c165185a70f44d98
maxhe <m.iurceac@gmail.com>
2023-03-17 15:59:34 +0200

    Initial commit

---
+++ src/main/java/org/example/homework_nr_1/HomeWork1.java
@@ -0,0 +1,13 @@
+package org.example.homework_nr_1;
+
+public class HomeWork1 {
+    public static void main(String[] args){
+    System.out.println("Hello world!");
+
+
+    long Current_World_Population = 7811080815;
+
+    System.out.println(Current_World_Population);
+}
+
+
