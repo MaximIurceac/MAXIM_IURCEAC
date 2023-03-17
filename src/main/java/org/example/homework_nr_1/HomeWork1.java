77d101537aa5e533ae85b0964253a43c9fed004b
maxhe <m.iurceac@gmail.com>
2023-03-17 16:57:41 +0200

    1/3 of work

--- src/main/java/org/example/homework_nr_1/HomeWork1.java
+++ src/main/java/org/example/homework_nr_1/HomeWork1.java
@@ -1,13 +1,21 @@
 package org.example.homework_nr_1;
 
 public class HomeWork1 {
-    public static void main(String[] args){
-    System.out.println("Hello world!");
+    public static void main(String[] args) {
+    System.out.println("Hellow World");
 
+        long CurrentWorldPopulation = 7811080815L;
+        int CarsProducedThisYear = 55235358;
+        char FirstLetterInAlphabet = 'A';
 
-    long Current_World_Population = 7811080815;
+        System.out.println(CurrentWorldPopulation);
+        System.out.println(CarsProducedThisYear);
+        System.out.println(FirstLetterInAlphabet);
+        System.out.println(CurrentWorldPopulation);
 
-    System.out.println(Current_World_Population);
+    }
 }
+
+
 
 
