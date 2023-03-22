9ce2b89ceee2951f43bbe7d17fd4b6b47465ee1e
maxhe <m.iurceac@gmail.com>
2023-03-22 21:50:23 +0200

    Start of HomeWork2

--- src/main/java/org/example/homework_nr_2/HomeWork2.java
+++ src/main/java/org/example/homework_nr_2/HomeWork2.java
@@ -52,13 +52,7 @@ public class HomeWork2 {
            System.out.println(counter);
        }
 
-        int number = 3; // Возводимое в степень число
-        int result = 1; // Результат возведения в степень
-        int power = 1; // Начальный показатель степени
-        while(power <= 10) { // условие входа в цикл
-            result = result * number;
-            System.out.println(number + " в степени " + power + " = " + result);
-            power++;
+
         }
     }
        }
