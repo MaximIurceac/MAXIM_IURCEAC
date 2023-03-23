ce1969d2b4175819e7bd3125b0c0f9a0813f8d36
Kefteme <m.iurceac@gmail.com>
2023-03-23 13:19:27 +0200

    Finish the second homework

--- src/main/java/org/example/homework_nr_2/HomeWork2.java
+++ src/main/java/org/example/homework_nr_2/HomeWork2.java
@@ -2,7 +2,10 @@ package org.example.homework_nr_2;
 
 public class HomeWork2 {
     public static void main(String[] args){
-       int months = 4;
+
+        System.out.println("В зависимости от числа отображается месяц");
+
+        int months = 4;
 
        switch (months){
            case 1:
@@ -46,16 +49,42 @@ public class HomeWork2 {
                System.out.println("Такого месяца нет");
 
        }
+           System.out.println("Все числа от 100 до 1000 которые делятся на 5");
+
+
        for(int counter = 100; counter <=1000; counter ++) {
            if(counter % 5 != 0)
                continue;
+
            System.out.println(counter);
        }
 
-
+        double sum = 0.0;
+        for (double n = 1; n <= 97; n += 2) {
+            sum += n / (n + 2);
         }
+        System.out.println("Сумма следующей серии: 1/3 + 3/5 + 5/7 + 7/9 + 9/11 + 11/13 + ... + 95/97 + 97/99");
+
+        System.out.println(sum);
+
+
+        int rows = 9;
+
+        System.out.println("Пирамида");
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println("");
+        }
+
     }
-       }
+
+}
+
+
 
 
 
