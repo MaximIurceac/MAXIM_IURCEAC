ecb8cff4ee51606a3638c9bbeb8443d97a959fbe
Kefteme <m.iurceac@gmail.com>
2023-03-27 19:09:34 +0300

    Finished 3.0

--- src/main/java/org/example/homework_nr_3/Circle.java
+++ src/main/java/org/example/homework_nr_3/Circle.java
@@ -18,28 +18,4 @@ class Circle {
     }
 }
 
-class TemperatureConverter{
-
-    double celsius;
-    double fahrenheit;
-
-    TemperatureConverter(double celsius, double fahrenheit){
-    this.celsius = celsius;
-    this.fahrenheit = fahrenheit;
-    }
-    public double toCelsius() {
-    return (fahrenheit - 32) / 1.8;
-    }
-    public double toFahrenheit() {
-        return celsius * 1.8 + 32;
-    }
-    public static void main(String[] args){
-        TemperatureConverter celsius = new TemperatureConverter(0, 123);
-        TemperatureConverter fahrenheit = new TemperatureConverter(232.78, 0 );
-        double celdegree = celsius.toCelsius();
-        double fahdegree = fahrenheit.toFahrenheit();
-        System.out.println("В цельсиях: " + celdegree + " В форенгейтах: " + fahdegree);
-    }
-}
-
 
