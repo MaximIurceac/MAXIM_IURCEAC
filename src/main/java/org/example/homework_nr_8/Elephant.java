d32066da0e3746cff4ce8fdfa9e26310f41acf72
Kefteme <m.iurceac@gmail.com>
2023-04-21 15:30:04 +0300

    Finished the homework 8

---
+++ src/main/java/org/example/homework_nr_8/Elephant.java
@@ -0,0 +1,43 @@
+package org.example.homework_nr_8;
+
+public class Elephant extends Erbivor implements Animal {
+
+    public static void main(String[] args) {
+        Animal elephant = new Elephant();
+        Erbivor elephant1 = new Elephant();
+
+
+
+
+        elephant.eat();
+        elephant.run();
+        elephant.drink();
+        Animal.sit();
+        elephant1.sleep();
+        elephant1.move();
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Elephant is eating");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Elephant is running");
+
+    }
+
+    @Override
+    public void drink() {
+        System.out.println("Elephant is drinking");
+
+    }
+
+
+    @Override
+    public void sleep() {
+        System.out.println("Erbivor is sleeping");
+    }
+}
+
