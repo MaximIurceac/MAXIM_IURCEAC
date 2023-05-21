e8f4b899c0ead462b8fd427b91014faff3617439
Kefteme <m.iurceac@gmail.com>
2023-05-21 20:24:08 +0300

    Finished the homework 14

---
+++ src/main/java/org/example/homework_nr_14/Triple.java
@@ -0,0 +1,35 @@
+package org.example.homework_nr_14;
+
+import java.io.Serializable;
+
+public class Triple <T extends Comparable<T>, V extends Animal & Serializable, K extends Number>{
+
+    private T tValue;
+    private V vValue;
+    private K kValue;
+
+    public Triple(T tValue, V vValue, K kValue) {
+        this.tValue = tValue;
+        this.vValue = vValue;
+        this.kValue = kValue;
+    }
+
+    public T returnTValue() {
+       return tValue;
+    }
+
+    public V returnVValue() {
+        return vValue;
+
+    }
+
+    public K returnKValue() {
+        return kValue;
+    }
+
+    public void printAllValues(){
+        System.out.println("T value " + tValue + "is of type " + tValue.getClass().getSimpleName());
+        System.out.println("V value " + vValue + "is of type " + vValue.getClass().getSimpleName());
+        System.out.println("K value " + kValue + "is of type " + kValue.getClass().getSimpleName());
+    }
+}
